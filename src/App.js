5e69d2b69f1b346909d7749ecae859fc8bc17763
BharatNK <bharatkattegoudar@gmail.com>
2024-01-05 12:39:21 +0530

    Quict start React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import MyButton from './components/MyButton';
+
+// function MyButton()
+// {
+//   return (
+//     <button> I am a Button</button>
+//   );
+// }
+
+const products = [
+  {name: 'Apple' ,id: 1,isfruit: true},
+  {name: 'Banana' ,id: 2,isfruit: false}
+];
+
+const Display = products.map( product =>
+  <li key= {product.id}>
+  {product.name}
+
+  </li>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>I am a heading</h1>
+      <MyButton/>
+      {Display}
     </div>
   );
 }
